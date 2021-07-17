64d0851727dc3aaf46efd48d0f486bdae6a7aef8
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-07-17 18:01:39 -0500

    18.1.8 test the api and connect the front-end form

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -51,6 +51,23 @@ const handlePizzaSubmit = event => {
   }
 
   const formData = { pizzaName, createdBy, size, toppings };
+//18.1.8 step ONE add the ability to POST the form data to the API
+  fetch('/api/pizzas', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => response.json())
+    .then(postResponse => {
+      alert('Pizza created successfully!');
+      console.log(postResponse);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
